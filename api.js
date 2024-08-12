c186a2ff111f785a41713fb32affa572d1708a46
tanish2007 <tanishgupta2007@gmail.com>
2024-08-12 16:38:20 +0530

    first commit

---
+++ api.js
@@ -0,0 +1,9 @@
+//*********api :- API STANDS FOR API(application Programming Interface)
+//which is a set of definitions of data and protocols.
+
+//Note:- API is not only set of data but it can have protocols 
+// method implementation etc.
+
+//Data in API is in the form of json(java script object notation)
+
+
